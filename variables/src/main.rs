2d1ea8c933c5c697ee72c0555998900fee2ba8bc
michaeldroz <michael.droz@gmail.com>
2022-08-28 15:47:43 -0600

    Part 2 of varialbes exercise

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,4 +2,6 @@ fn main() {
     let missiles = 8; 
     let ready = 2; 
     println!("Firing {} of {} missiles...", ready, missiles);
+    missiles = missiles - ready; 
+    println!("{} missiles left", missiles); 
 }
---
+++ variables/target/debug/deps/variables-37444c7b95de8efa.d
@@ -0,0 +1,5 @@
+/Users/michaeldroz/Rust/myExercises/variables/target/debug/deps/variables-37444c7b95de8efa: src/main.rs
+
+/Users/michaeldroz/Rust/myExercises/variables/target/debug/deps/variables-37444c7b95de8efa.d: src/main.rs
+
+src/main.rs:
